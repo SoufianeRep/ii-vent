3495424b9102a3ad7f0fcdc623df2a567ca93247
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-23 23:15:18 +0900

    fixed the navbar style

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,13 +1,10 @@
 class EventPolicy < ApplicationPolicy
   class Scope < Scope
-
-  end
-  
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-
+    def resolve
+      scope.all
+    end
+  end
 
   def show?
     true
