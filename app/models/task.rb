b5d5b9f61f2af18746c74639dde0794a3a4d4397
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-23 12:18:11 +0900

    finished validations and added 2 migrations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   belongs_to :event
   belongs_to :task, optional: true
   has_many :messages, as: :room
+  has_many :task_members, dependent: :destroy
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :status, presence: true
+  enum status: { pending: "pending", done: "done", archived: "archived" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :event_members
+  has_many :event_members, dependent: :destroy
   has_many :events, through: :event_members
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :location, presence: true
+  validates :role, presence: true
+  enum role: { staff: "staff", organizer: "organizer", artist: "artist", security: "security" }
 end
