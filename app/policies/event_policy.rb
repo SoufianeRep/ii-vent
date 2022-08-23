fa55d2374dd66879ed59d850de45fe390384959e
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-23 22:51:34 +0900

    styled the tasks section and dashboard_task partial for dashboard

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,13 +1,12 @@
 class EventPolicy < ApplicationPolicy
   class Scope < Scope
-
+    def resolve
+      # scope.where(user: user)
+      scope.all
+    end
   end
-  
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
 
+  # NOTE: Be explicit about which records you allow access to!
 
   def show?
     true
