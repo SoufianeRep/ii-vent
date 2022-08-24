9265d2057c301ce7b6aef983184e1e1e65e1f5da
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-25 02:12:50 +0900

    fixed tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,6 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 
 // To enable bootstrap tooltip on all pages
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList];
-tooltipList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+tooltipList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
