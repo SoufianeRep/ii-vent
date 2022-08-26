87d5137e01998ffddd9ca11d942f37cf38ec3445
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-26 14:14:21 +0900

    made the task modular for better display

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tasks"
 export default class extends Controller {
-  static targets = ['task'];
+  static targets = ['task', 'badge', 'members'];
 
   connect() {
     console.log('hello from tasks controller');
   }
 
   toggleDisplay(e) {
-    this.taskTarget.classList.toggle('d-none')
+    this.taskTarget.classList.toggle('d-none');
+    this.badgeTarget.classList.toggle('d-none');
+    this.membersTarget.classList.toggle('d-none');
   }
 }
