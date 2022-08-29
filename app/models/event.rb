d8a60d7cb03e8c4ff5289527693c04f8027a5891
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-29 15:34:26 +0900

    styled calendar page more - needs more work for colors

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,4 +18,12 @@ class Event < ApplicationRecord
   def start_time
     self.start_date
   end
+
+  def end_time
+    self.end_date
+  end
+
+  def time
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  end
 end
