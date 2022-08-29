967b2c2231a73fc523b8ddeeb3096d93a97ad8be
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-29 23:16:30 +0900

    attempt to mark task as done from Dashboard - no luck, stuck at fetch request

--- app/javascript/controllers/dashboard_mark_task_as_done_controller.js
+++ app/javascript/controllers/dashboard_mark_task_as_done_controller.js
@@ -2,17 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dashboard-mark-task-as-done"
 export default class extends Controller {
-  static targets = ["status"]
+  static targets = ["form", "status"]
 
   connect() {
-    console.log("Hello from the 'dashboard-mark-task-as-done' controller!");
+    // console.log("Hello from the 'dashboard-mark-task-as-done' controller!");
+    // console.log(this.formTarget);
+    // console.log(this.statusTarget);
   }
 
-  markAsDone() {
-    console.log("Changed!");
+  markAsDone(event) {
+    event.preventDefault()
+    console.log(this.formTarget.action);
+    // url = this.formTarget.action;
+    // options = {
+    //   method: 'PATCH',
+    //   header: { "Accept": 'text/html' },
+    //   body: new FormData(this.formTarget)
+    // }
 
-    console.log(this.statusTarget);
-
-
+    // fetch(url, options)
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
   }
 }
