406682cde94fb442f3c4fbdac164e57edc5aaa3e
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-29 14:13:07 +0900

    moved calendar to /calendar instead of inside dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "/dashboard", to: "users#dashboard", as: :dashboard
+  get "/calendar", to: "users#calendar", as: :calendar
   resources :events, only: [:new, :create, :show] do
     resources :event_members, only: [:create]
     resources :tasks, only: [:new, :create] do
