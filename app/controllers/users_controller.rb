fd00834b14a48fe4c841427e5416aab32253b89a
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-30 23:05:40 +0900

    more small tweaks - time now matches event show (no longer dynamic local time - hardcoded JST)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = current_user
     authorize @user
     if user_signed_in?
-      @events = policy_scope(Event).order(start_date: :asc)
+      @events = policy_scope(Event).where('start_date > ?', Date.yesterday).order(start_date: :asc)
       @tasks = policy_scope(Task).where(done: false).order(end: :asc)
 
       # Scope your query to the dates being shown:
@@ -36,6 +36,5 @@ class UsersController < ApplicationController
     else
       redirect_to root_path, alert: "You are not authorized to perform this action."
     end
-
   end
 end
