13797ac8ed452cfe45aeb07b702814fc9eeb27a5
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-30 11:44:18 +0900

    functioning chat and styling for the general event room

--- app/javascript/controllers/event_messages_subscription_controller.js
+++ app/javascript/controllers/event_messages_subscription_controller.js
@@ -3,8 +3,8 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="event-messages-subscription"
 export default class extends Controller {
-  static targets = [ "messages" ]
-  static values = { eventId: Number } // string?
+  static targets = [ "messages", "form", "panel" ]
+  static values = { eventId: Number, currentUserId: Number } // string?
 
   connect() {
     console.log(`Connecting to the channel: ${this.eventIdValue}`)
@@ -12,15 +12,46 @@ export default class extends Controller {
       { channel: "EventMessagesChannel", id: this.eventIdValue },
       {
         received: (data) => {
-          this.#insertMessage(data)
+          this.#insertMessageAndScrollDown(data)
         },
       }
     )
   }
 
   // Private methods
-  #insertMessage(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  #insertMessageAndScrollDown(data) {
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+
+    // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+    // Inserting the `message` in the DOM
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.panelTarget.scrollTo(0, this.panelTarget.scrollHeight)
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-start" : "justify-content-end"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  // we need a reset form private method to clear the form after submit
+  resetForm(event) {
+    console.log("hello")
+    this.formTarget.reset()
   }
 }
