80620f204915e75689cfab81e05d4e400e8674c9
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-31 14:00:05 +0900

    finished some styling

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,7 +34,7 @@ class EventsController < ApplicationController
       redirect_to event_path(@event)
     else
       build_event_members
-      render :new
+      redirect_to new_event_path, alert: "Missing field input"
     end
   end
 
